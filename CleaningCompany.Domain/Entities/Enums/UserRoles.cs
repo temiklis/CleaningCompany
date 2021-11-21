1e33d13f914aee9cc3bbb858b9f83e36b9d78308
temiklis31@gmail.com <temiklis31@gmail.com>
2021-11-21 22:05:08 +0300

    feat: add ApplicationDbSeed.

---
+++ CleaningCompany.Domain/Entities/Enums/UserRoles.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace CleaningCompany.Domain.Entities.Enums
+{
+    public enum UserRoles
+    {
+        [Description("Admin")]
+        Admin,
+        [Description("Employee")]
+        Employee,
+        [Description("User")]
+        User
+    }
+}
